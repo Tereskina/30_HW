544d2c7840de0514f973411d9ff2ef8effb90b62
Tereskina <iv.kseni2@gmail.com>
2023-03-14 12:03:49 +0000

    Alter user model, add selection

--- HW_30/settings.py
+++ HW_30/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'ads',
     'users'
 ]
@@ -158,6 +159,11 @@ TOTAL_ON_PAGE = 5
 # }
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5
 }
+
+AUTH_USER_MODEL = 'users.User'
--- ads/views/ad.py
+++ ads/views/ad.py
@@ -2,21 +2,51 @@ import json
 
 
 from django.db.models import Q
-from django.http import JsonResponse, Http404
+from django.http import JsonResponse
 
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
-from rest_framework.generics import ListAPIView
+from django.views.generic import CreateView, UpdateView, DeleteView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
+from ads.models import Ad, Selection
+from ads.permissions import SelectionUpdatePermission
 
-from ads.models import Ad
-
-from ads.serializers import AdListSerializer, AdPostSerializer, AdUpdateSerializer
+from ads.serializers import AdListSerializer, AdPostSerializer, AdUpdateSerializer, SelectionSerializer, \
+    SelectionDetailSerializer, SelectionListSerializer, AdDetailSerializer
 
 
 def index(request) -> JsonResponse:
     return JsonResponse({"status": "ok"}, status=200)
+
+
+class SelectionListView(ListAPIView):
+    queryset = Selection.objects.all()
+    serializer_class = SelectionListSerializer
+
+
+class SelectionRetrieveView(RetrieveAPIView):
+    queryset = Selection.objects.all()
+    serializer_class = SelectionDetailSerializer
+
+
+class SelectionCreateView(CreateAPIView):
+    queryset = Selection.objects.all()
+    serializer_class = SelectionSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class SelectionUpdateView(UpdateAPIView):
+    queryset = Selection.objects.all()
+    serializer_class = SelectionSerializer
+    permission_classes = [IsAuthenticated, SelectionUpdatePermission]
+
+
+class SelectionDeleteView(DestroyAPIView):
+    queryset = Selection.objects.all()
+    serializer_class = SelectionSerializer
+    permission_classes = [IsAuthenticated, SelectionUpdatePermission]
 
 
 class AdListView(ListAPIView):
@@ -47,24 +77,10 @@ class AdListView(ListAPIView):
             else:
                 search_query |= Q(author__location__name__icontains=location)
 
-
         if price_from := request.GET.get('price_from', None):
             self.queryset = self.queryset.filter(price__gte=price_from)
         if price_to := request.GET.get('price_to', None):
             self.queryset = self.queryset.filter(price__lte=price_to)
-        # price_from = request.GET.get('price_from', None)
-        # if price_from:
-        #     if search_query is None:
-        #         search_query = Q(price__gte=price_from)
-        #     else:
-        #         search_query &= Q(price__gte=price_from)
-        #
-        # price_to = request.GET.get('price_to', None)
-        # if price_to:
-        #     if search_query is None:
-        #         search_query = Q(price__lte=price_to)
-        #     else:
-        #         search_query &= Q(price__lte=price_to)
 
         if search_query:
             self.queryset = self.queryset.select_related('author').prefetch_related('category').filter(search_query).\
@@ -87,16 +103,10 @@ class AdCreateView(CreateView):
         return JsonResponse(serializer.errors, safe=False, status=422)
 
 
-class AdDetailView(DetailView):
-    model = Ad
-
-    def get(self, request, *args, **kwargs):
-        try:
-            super(AdDetailView, self).get(request, *args, **kwargs)
-        except Http404 as error:
-            return JsonResponse({'error': error.args}, status=404)
-        ads_serializer = AdListSerializer(self.object)
-        return JsonResponse(ads_serializer.data, safe=False, status=200)
+class AdDetailView(RetrieveAPIView):
+    queryset = Ad.objects.all()
+    serializer_class = AdDetailSerializer
+    permission_classes = [IsAuthenticated]
 
 
 @method_decorator(csrf_exempt, name="dispatch")
